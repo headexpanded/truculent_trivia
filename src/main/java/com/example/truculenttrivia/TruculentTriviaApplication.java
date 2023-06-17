598c8da6ce67d7195e8bff6cbe70bbd33e7cf358
moncon <markh@bellemoncon>
2023-06-17 15:46:46 +0200

    initial commit

---
+++ src/main/java/com/example/truculenttrivia/TruculentTriviaApplication.java
@@ -0,0 +1,13 @@
+package com.example.truculenttrivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruculentTriviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruculentTriviaApplication.class, args);
+	}
+
+}
